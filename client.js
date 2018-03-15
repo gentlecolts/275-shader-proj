df21527d4e07cba3707d47aad54c1e9c6fe406ed
SamaraPainter <35310274+SamaraPainter@users.noreply.github.com>
2018-03-15 13:50:43 -0400

    adding initial client script

---
+++ client.js
@@ -0,0 +1,62 @@
+var shaderID = 0;
+
+function save() {
+	var shader = $('#shadercode').value;
+	var URL = "http://localhost:8080/save";
+	$.ajax({
+		type: "POST",
+		url: URL,
+		dataType: "jsonp",
+		data: {
+			"shader": shader
+		},
+		success: function(msg) {
+			if (msg.success) {
+				shaderID = msg.id;
+			} else {
+				
+			}
+		},
+		error: function(jgXHR, textStatus, thrownError) {
+			alert("Error: " + textStatus + " " + thrownError);
+			return;
+		}
+	});
+}
+
+function homePage() {
+	var URL = "http://localhost:8080/home";
+	$.ajax({
+		type: "GET",
+		url: URL,
+		dataType: "html",
+		success: function(msg) {
+			$('#pagecontent').html(msg);
+		},
+		error: function(jgXHR, textStatus, thrownError) {
+			alert("Error: " + textStatus + " " + thrownError);
+			return;
+		}
+	});
+}
+
+function editorPage() {
+	var URL = "http://localhost:8080/editor";
+	$.ajax({
+		type: "GET",
+		url: URL,
+		dataType: "html",
+		success: function(msg) {
+			$('#pagecontent').html(msg);
+			var codeMir = CodeMirror($('#shadercode'));
+		},
+		error: function(jgXHR, textStatus, thrownError) {
+			alert("Error: " + textStatus + " " + thrownError);
+			return;
+		}
+	});
+}
+
+$(document).ready(function() {
+	homePage();
+}
