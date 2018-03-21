e7e9e25eb1117626bcf6a350a1f8b6e6fce867ff
Jack Phoebus <jmp1996.jp@gmail.com>
2018-03-21 14:26:23 -0400

    skeleton for shader code

--- ServeShaders.js
+++ ServeShaders.js
@@ -8,66 +8,17 @@ const fs=require('fs');
 const util = require('util');
 
 var EventEmitter=require('events').EventEmitter;
+var root="./shaders"
 
-const key=fs.readFileSync("../apikey.txt","utf8").trim();
-const geturl="https://api.wunderground.com/api/"+key+"/hourly/q/";
-//console.log(geturl);
-
-exports.render=function(){
+exports.getAll=function(){
 	return fs.readFileSync("./weather.html");
 }
 
-function escapeXML(unsafe) {
-	return unsafe.replace(/[<>&'"]/g, function (c) {
-		switch (c) {
-			case '<': return '&lt;';
-			case '>': return '&gt;';
-			case '&': return '&amp;';
-			case '\'': return '&apos;';
-			case '"': return '&quot;';
-		}
-	});
+exports.getShader=function(id){
 }
 
-class Weather extends EventEmitter{
-	constructor(){
-		super();
-	}
-	
-	fetchWeather(zip){
-		request({
-			url:geturl+zip+".json",
-			json:true
-		},(err,response,data)=>{
-			//store hourly conveniently
-			var hourly=data.hourly_forecast;
-			
-			try{
-				//make the table
-				var str="<table data-role='table' class='ui-responsive'><tbody>";
-			
-				//add content to table
-				for(var i=0;i<hourly.length;i++){
-					//make row and cell
-					str+="<tr><td>";
-				
-					//add text and icon, then close
-					str+=escapeXML(hourly[i].FCTTIME.pretty);
-					str+="<img src='"+encodeURI(hourly[i].icon_url)+"'></img></td></tr>";
-				}
-			
-				//close tags
-				str+="</tbody></table>";
-				this.emit("done",str);
-			}catch(e){
-				this.emit("done","Error: bad response from wunderground, maybe bad zipcode?");
-			}
-		});
-	}
+exports.newShader=function(){
 }
 
-exports.getWeather=function(zip,callback){
-	var w=new Weather();
-	w.once('done',callback);
-	w.fetchWeather(zip);
+exports.saveShader=function(id,contents){
 }
