a6aea57ecd32de23589412d7e474181001ee2ef7
Jack Phoebus <jmp1996.jp@gmail.com>
2018-03-21 14:02:40 -0400

    added my hw4 server
    rip this apart and use it for our server pls thanks

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+app.use(express.static("."));
+const calc=require("./Calculator");
+const weather=require("./Weather");
+
+app.listen(8080,function(){
+	console.log("live");
+});
+
+app.get("/calc",function(req,res){
+	res.send(calc.render());
+});
+
+app.get("/sum",function(req,res){
+	res.send(calc.sum(req.query.n));
+});
+
+app.get("/fact",function(req,res){
+	res.send(calc.factorial(req.query.n));
+});
+
+app.get("/weather",function(req,res){
+	res.send(weather.render());
+});
+
+app.get("/hourly",function(req,res){
+	weather.getWeather(req.query.zip,function(data){
+		//console.log(data);
+		res.send(data);
+	});
+});
