2a88665126a9ebfc79d6d4706aac62f8150366ac
Andrew Yaros <aeyaros@gmail.com>
2018-03-21 16:07:48 -0400

    updated client, homepage
    added correct function calls in populate previews in client, new shader button on homepage

--- client.js
+++ client.js
@@ -64,7 +64,7 @@ function populatePreviews() {
 			for (i = 0; i < shaders.length; i++) //each row has one element; the shader in question
 			{
 				str += "<tr><td>"
-					+ "<button class=\"button buttonlist\">"
+					+ "<button onclick=\"loadshader("shaders[i]\")" class=\"button buttonlist\">"
 					+ shaders[i]
 					+ "</button>"
 					+ "</td></tr>";
